b8a1536dbc9d2c80339dd4bd9c6ba226b033bdc0
disseminate <kwindsor@uwo.ca>
2015-03-29 15:37:39 -0400

    Started video changing Changing name

--- client.js
+++ client.js
@@ -9,9 +9,32 @@ socket.on( "chat", function( data ) {
 	$( "#chat-list" ).append( $( "<li>" ).append( $( "<b>" ).text( data[0] ), ": ", $( "<span />" ).text( data[1] ) ) );
 } );
 
+var player;
+
+var playerURLs = { };
+var playerTimes = { };
+
+function onYouTubeIframeAPIReady() {
+	if( player == null ) {
+		player = new YT.Player( "video", {
+			height: '480',
+			width: '848',
+			videoId: 'N1uiLR6luWo',
+			events: {
+				"onReady": playVid
+			}
+		} );
+	}
+}
+
+function playVid( ev ) {
+	ev.target.playVideo();
+}
+
 $( document ).ready( function() {
 	$( "#room-entry-entry" ).val( room );
 	$( "#room-name" ).text( room );
+	$( "#chat-name" ).val( user );
 	
 	$( "#room-entry" ).submit( function( ev ) {
 		room = $( "#room-entry-entry" ).val();
@@ -23,11 +46,41 @@ $( document ).ready( function() {
 		ev.preventDefault();
 	} );
 	
+	$( "#video-url-submit" ).click( function() {
+		if( $( "#video-url" ).val().length > 0 ) {
+			socket.emit( "change-video", $( "#video-url" ).val() );
+		}
+	} );
+	
 	$( "#chat-entry" ).keyup( function( ev ) {
 		if( ev.keyCode == 13 ) {
-			socket.emit( "chat", $( this ).val() ); // .text( ": " + $( this ).val() )
-			$( "#chat-list" ).append( $( "<li>" ).append( $( "<b>" ).text( user ), ": ", $( "<span />" ).text( $( this ).val() ) ) );
-			$( this ).val( "" );
+			var a = $( this ).val();
+			if( a.length > 0 ) {
+				socket.emit( "chat", a );
+				$( "#chat-list" ).append( $( "<li>" ).append( $( "<b>" ).text( user ), ": ", $( "<span />" ).text( a ) ) );
+				$( this ).val( "" );
+				$( "#chat" ).scrollTop( $( "#chat" ).prop( "scrollHeight" ) );
+			}
+		}
+	} );
+	
+	$( "#chat-entry" ).width( $( "#chat" ).width() + 40 );
+	$( window ).resize( function() {
+		$( "#chat-entry" ).width( $( "#chat" ).width() + 40 );
+	} );
+	
+	$( "#chat-name" ).keyup( function( ev ) {
+		if( ev.keyCode == 13 && $( this ).val().length > 0 ) {
+			user = $( this ).val();
+			socket.emit( "change-user", user );
+			$( this ).blur();
+		}
+	} );
+	
+	$( "#chat-name-submit" ).click( function() {
+		if( $( "#chat-name" ).val().length > 0 ) {
+			user = $( "#chat-name" ).val();
+			socket.emit( "change-user", user );
 		}
 	} );
 } );
--- index.js
+++ index.js
@@ -16,24 +16,45 @@ http.listen( 80, function() {
 	console.log( "popcornwith.me initialized." );
 } );
 
+var playerURLs = { };
+var playerTimes = { };
+
 io.on( "connection", function( socket ) {
 	var chatName = "Guest " + Math.floor( Math.random() * 10000 );
+	var room = "";
 	
 	socket.on( "change-room", function( data ) {
-		for( var i = 0; i < socket.rooms.length; i++ ) {
-			socket.leave( socket.rooms[i] );
+		if( data.length > 0 ) {
+			for( var i = 0; i < socket.rooms.length; i++ ) {
+				socket.leave( socket.rooms[i] );
+			}
+			socket.join( data );
+			room = data;
 		}
-		socket.join( data );
+	} );
+	
+	socket.on( "change-video", function( data ) {
+		if( data.length > 0 ) {
+			for( var i = 0; i < socket.rooms.length; i++ ) {
+				socket.broadcast.to( socket.rooms[i] ).emit( "change-video", data );
+				playerURLs[socket.rooms[i]] = data;
+				playerTimes[socket.rooms[i]] = 0;
+			}
+		}
 	} );
 	
 	socket.on( "change-user", function( data ) {
-		chatName = data;
+		if( data.length > 0 ) {
+			chatName = data;
+		}
 	} );
 	
 	socket.on( "chat", function( data ) {
-		console.log( chatName + ": " + data );
-		for( var i = 0; i < socket.rooms.length; i++ ) {
-			socket.broadcast.to( socket.rooms[i] ).emit( "chat", [ chatName, data ] );
+		if( data.length > 0 ) {
+			console.log( chatName + ": " + data );
+			for( var i = 0; i < socket.rooms.length; i++ ) {
+				socket.broadcast.to( socket.rooms[i] ).emit( "chat", [ chatName, data ] );
+			}
 		}
 	} );
 } );
